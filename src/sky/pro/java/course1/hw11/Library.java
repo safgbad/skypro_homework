3c2663602f4b4071a4f74b8a1c1e2422e27d0451
Artem Nazarov <art.nazar.11@gmail.com>
2022-10-24 11:55:54 +0300

    added braces

--- src/sky/pro/java/course1/hw11/Library.java
+++ src/sky/pro/java/course1/hw11/Library.java
@@ -19,25 +19,27 @@ public class Library {
 
     public void printBooks() {
         for (Book book : bookArray) {
-            if (book != null)
+            if (book != null) {
                 System.out.printf("%s %s: %s: %d\n", book.getAuthor().getFirstName(), book.getAuthor().getLastName(),
                         book.getName(), book.getYear());
+            }
         }
     }
 
     private int indexOf(String name) {
         for (int i = 0; i < bookArray.length; i++) {
-            if ((bookArray[i] != null) && (bookArray[i].getName().equalsIgnoreCase(name)))
+            if ((bookArray[i] != null) && (bookArray[i].getName().equalsIgnoreCase(name))) {
                 return i;
+            }
         }
         return -1;
     }
 
     public void printBookInformationByName(String name) {
         int index = indexOf(name);
-        if (index == -1)
+        if (index == -1) {
             System.out.printf("The %s hasn't found in library\n", name);
-        else {
+        } else {
             System.out.printf("%s by %s %s was published in %d\n", bookArray[index].getName(),
                     bookArray[index].getAuthor().getFirstName(), bookArray[index].getAuthor().getLastName(),
                     bookArray[index].getYear());
@@ -46,10 +48,11 @@ public class Library {
 
     public boolean setYear(String name, int year) {
         int index = indexOf(name);
-        if (index == -1)
+        if (index == -1) {
             return false;
-        else
+        } else {
             bookArray[index].setYear(year);
+        }
         return true;
     }
 }
