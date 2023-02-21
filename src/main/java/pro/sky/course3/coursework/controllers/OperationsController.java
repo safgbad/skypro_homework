0d1a1c4254c82c13d0b9496e46a28b5959a92c91
Artem Nazarov <art.nazar.11@gmail.com>
2023-02-21 13:39:05 +0300

    documented with swagger

--- src/main/java/pro/sky/course3/coursework/controllers/OperationsController.java
+++ src/main/java/pro/sky/course3/coursework/controllers/OperationsController.java
@@ -1,6 +1,12 @@
 package pro.sky.course3.coursework.controllers;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.Parameters;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.core.io.InputStreamResource;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
@@ -25,6 +31,7 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.List;
 
+@Tag(name = "Операции с носками", description = "Импорт/экспорт")
 @RestController
 @RequestMapping("/api/socks/operations")
 public class OperationsController {
@@ -37,6 +44,29 @@ public class OperationsController {
         this.operationsService = operationsService;
     }
 
+    @io.swagger.v3.oas.annotations.Operation(
+            summary = "Экспорт операций с носками"
+    )
+    @ApiResponses(value = {
+            @ApiResponse(
+                    responseCode = "200",
+                    content = {
+                            @Content(mediaType = MediaType.APPLICATION_JSON_VALUE),
+                    },
+                    description = "JSON-файл успешно сгенерирован и экспортирован"
+            ),
+            @ApiResponse(
+                    responseCode = "204",
+                    description = "Нет ни одной операции с носками"
+            ),
+            @ApiResponse(
+                    responseCode = "500",
+                    content = {
+                            @Content(mediaType = "text/plain")
+                    },
+                    description = "Внутренняя ошибка сервера"
+            )
+    })
     @GetMapping("/export")
     public ResponseEntity<Object> downloadOperationsDataFile() {
         try {
@@ -58,6 +88,42 @@ public class OperationsController {
         }
     }
 
+    @io.swagger.v3.oas.annotations.Operation(
+            summary = "Импорт операций с носками",
+            description = "Импортированные операции заменяют все имеющиеся," +
+                    "операции синхронизируются с данными о наличии носков на складе"
+    )
+    @Parameters(value = {
+            @Parameter(
+                    name = "file",
+                    content = {
+                            @Content(mediaType = MediaType.MULTIPART_FORM_DATA_VALUE)
+                    }
+            )
+    })
+    @ApiResponses(value = {
+            @ApiResponse(
+                    responseCode = "200",
+                    content = {
+                            @Content(mediaType = "text/plain")
+                    },
+                    description = "Операции с носками успешно импортированы"
+            ),
+            @ApiResponse(
+                    responseCode = "400",
+                    content = {
+                            @Content(mediaType = "text/plain")
+                    },
+                    description = "Ошибка во входных данных"
+            ),
+            @ApiResponse(
+                    responseCode = "500",
+                    content = {
+                            @Content(mediaType = "text/plain")
+                    },
+                    description = "Внутренняя ошибка сервера"
+            )
+    })
     @PutMapping(path = "/import", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
     public ResponseEntity<String> uploadOperationsDataFile(@RequestParam MultipartFile file) {
         try {
@@ -68,12 +134,9 @@ public class OperationsController {
         } catch (NothingToImportException e) {
             return ResponseEntity.badRequest()
                     .body("Uploaded file is empty");
-        } catch (InvalidInputException | JsonProcessingException e) {
+        } catch (InvalidInputException | JsonProcessingException | NotEnoughSocksException e) {
             return ResponseEntity.badRequest()
-                    .body("Invalid file format");
-        } catch (NotEnoughSocksException e) {
-            return ResponseEntity.badRequest()
-                    .body("Error with socks quantity");
+                    .body(e.toString());
         } catch (IOException e) {
             return ResponseEntity.internalServerError()
                     .body(e.toString());
--- src/main/java/pro/sky/course3/coursework/controllers/SocksController.java
+++ src/main/java/pro/sky/course3/coursework/controllers/SocksController.java
@@ -1,6 +1,14 @@
 package pro.sky.course3.coursework.controllers;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.Parameters;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.core.io.InputStreamResource;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
@@ -14,6 +22,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
+import pro.sky.course3.coursework.dto.ApiPairOfSocksDTO;
 import pro.sky.course3.coursework.exceptions.InvalidInputException;
 import pro.sky.course3.coursework.exceptions.NotEnoughSocksException;
 import pro.sky.course3.coursework.exceptions.NothingToExportException;
@@ -22,7 +31,6 @@ import pro.sky.course3.coursework.model.PairOfSocks;
 import pro.sky.course3.coursework.model.enums.OperationType;
 import pro.sky.course3.coursework.services.OperationsService;
 import pro.sky.course3.coursework.services.SocksService;
-import pro.sky.course3.coursework.dto.ApiPairOfSocksDTO;
 
 import java.io.FileInputStream;
 import java.io.IOException;
@@ -30,6 +38,7 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.Map;
 
+@Tag(name = "Носки", description = "CRUD-операции, импорт/экспорт")
 @RestController
 @RequestMapping("/api/socks")
 public class SocksController {
@@ -42,7 +51,40 @@ public class SocksController {
         this.operationsService = operationsService;
     }
 
-    @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE)
+    @Operation(
+            summary = "Добавление партии носков",
+            description = "Партия состоит из носков одного цвета, размера и с одинаковым содержанием хлопка"
+    )
+    @Parameters(value = {
+            @Parameter(
+                    name = "apiPairOfSocksDTO",
+                    schema = @Schema(implementation = ApiPairOfSocksDTO.class)
+            )
+    })
+    @ApiResponses(value = {
+            @ApiResponse(
+                    responseCode = "200",
+                    content = {
+                            @Content(mediaType = "text/plain")
+                    },
+                    description = "Партия носков успешно добавлена"
+            ),
+            @ApiResponse(
+                    responseCode = "400",
+                    content = {
+                            @Content(mediaType = "text/plain")
+                    },
+                    description = "Ошибка во входных данных"
+            ),
+            @ApiResponse(
+                    responseCode = "500",
+                    content = {
+                            @Content(mediaType = "text/plain")
+                    },
+                    description = "Внутренняя ошибка сервера"
+            )
+    })
+    @PostMapping(path = "/json", consumes = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<String> addSocks(@RequestBody ApiPairOfSocksDTO apiPairOfSocksDTO) {
         try {
             PairOfSocks pairOfSocks = socksService.addSocks(apiPairOfSocksDTO);
@@ -58,13 +100,57 @@ public class SocksController {
         }
     }
 
+    @Operation(
+            summary = "Добавление партии носков",
+            description = "Партия состоит из носков одного цвета, размера и с одинаковым содержанием хлопка"
+    )
+    @Parameters(value = {
+            @Parameter(
+                    name = "color",
+                    example = "red"
+            ),
+            @Parameter(
+                    name = "size",
+                    example = "39"
+            ),
+            @Parameter(
+                    name = "cottonPart",
+                    example = "98"
+            ),
+            @Parameter(
+                    name = "quantity",
+                    example = "15"
+            )
+    })
+    @ApiResponses(value = {
+            @ApiResponse(
+                    responseCode = "200",
+                    content = {
+                            @Content(mediaType = "text/plain")
+                    },
+                    description = "Партия носков успешно добавлена"
+            ),
+            @ApiResponse(
+                    responseCode = "400",
+                    content = {
+                            @Content(mediaType = "text/plain")
+                    },
+                    description = "Ошибка во входных данных"
+            ),
+            @ApiResponse(
+                    responseCode = "500",
+                    content = {
+                            @Content(mediaType = "text/plain")
+                    },
+                    description = "Внутренняя ошибка сервера"
+            )
+    })
     @PostMapping
-    public ResponseEntity<String> addSocks(@RequestParam(required = false) String color,
+    public ResponseEntity<String> addSocksParam(@RequestParam(required = false) String color,
                                            @RequestParam(required = false) Integer size,
                                            @RequestParam(required = false) Integer cottonPart,
                                            @RequestParam(required = false) Integer quantity) {
         try {
-            // TODO: do another adding method
             PairOfSocks pairOfSocks = socksService.addSocks(new ApiPairOfSocksDTO(color, size, cottonPart, quantity));
             operationsService.addOperation(pairOfSocks, quantity, OperationType.RECEIVING);
             return ResponseEntity.ok()
@@ -78,6 +164,53 @@ public class SocksController {
         }
     }
 
+    @Operation(
+            summary = "Получить остаток носков на складе",
+            description = "Остаток можно получить по конкретным цвету и размеру и по интервалу содержания хлопка"
+    )
+    @Parameters(value = {
+            @Parameter(
+                    name = "color",
+                    example = "red"
+            ),
+            @Parameter(
+                    name = "size",
+                    example = "39"
+            ),
+            @Parameter(
+                    name = "cottonMin",
+                    example = "50"
+            ),
+            @Parameter(
+                    name = "cottonMax",
+                    example = "100"
+            )
+    })
+    @ApiResponses(value = {
+            @ApiResponse(
+                    responseCode = "200",
+                    content = {
+                            @Content(
+                                    schema = @Schema(type = "integer", format = "int64")
+                            )
+                    },
+                    description = "Остаток высчитан"
+            ),
+            @ApiResponse(
+                    responseCode = "400",
+                    content = {
+                            @Content(mediaType = "text/plain")
+                    },
+                    description = "Ошибка во входных данных"
+            ),
+            @ApiResponse(
+                    responseCode = "500",
+                    content = {
+                            @Content(mediaType = "text/plain")
+                    },
+                    description = "Внутренняя ошибка сервера"
+            )
+    })
     @GetMapping
     public ResponseEntity<Object> getSocks(@RequestParam(required = false) String color,
                                            @RequestParam(required = false) Integer size,
@@ -95,7 +228,40 @@ public class SocksController {
         }
     }
 
-    @PutMapping(consumes = MediaType.APPLICATION_JSON_VALUE)
+    @Operation(
+            summary = "Отпуск партии носков со склада",
+            description = "Партия состоит из носков одного цвета, размера и с одинаковым содержанием хлопка"
+    )
+    @Parameters(value = {
+            @Parameter(
+                    name = "apiPairOfSocksDTO",
+                    schema = @Schema(implementation = ApiPairOfSocksDTO.class)
+            )
+    })
+    @ApiResponses(value = {
+            @ApiResponse(
+                    responseCode = "200",
+                    content = {
+                            @Content(mediaType = "text/plain")
+                    },
+                    description = "Носки успешно отпущены со склада"
+            ),
+            @ApiResponse(
+                    responseCode = "400",
+                    content = {
+                            @Content(mediaType = "text/plain")
+                    },
+                    description = "Ошибка во входных данных"
+            ),
+            @ApiResponse(
+                    responseCode = "500",
+                    content = {
+                            @Content(mediaType = "text/plain")
+                    },
+                    description = "Внутренняя ошибка сервера"
+            )
+    })
+    @PutMapping(path = "/json", consumes = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<String> releaseSocks(@RequestBody ApiPairOfSocksDTO apiPairOfSocksDTO) {
         try {
             PairOfSocks pairOfSocks = socksService.releaseSocks(apiPairOfSocksDTO);
@@ -111,8 +277,53 @@ public class SocksController {
         }
     }
 
+    @Operation(
+            summary = "Отпуск партии носков со склада",
+            description = "Партия состоит из носков одного цвета, размера и с одинаковым содержанием хлопка"
+    )
+    @Parameters(value = {
+            @Parameter(
+                    name = "color",
+                    example = "red"
+            ),
+            @Parameter(
+                    name = "size",
+                    example = "39"
+            ),
+            @Parameter(
+                    name = "cottonPart",
+                    example = "98"
+            ),
+            @Parameter(
+                    name = "quantity",
+                    example = "15"
+            )
+    })
+    @ApiResponses(value = {
+            @ApiResponse(
+                    responseCode = "200",
+                    content = {
+                            @Content(mediaType = "text/plain")
+                    },
+                    description = "Носки успешно отпущены со склада"
+            ),
+            @ApiResponse(
+                    responseCode = "400",
+                    content = {
+                            @Content(mediaType = "text/plain")
+                    },
+                    description = "Ошибка во входных данных"
+            ),
+            @ApiResponse(
+                    responseCode = "500",
+                    content = {
+                            @Content(mediaType = "text/plain")
+                    },
+                    description = "Внутренняя ошибка сервера"
+            )
+    })
     @PutMapping
-    public ResponseEntity<String> releaseSocks(@RequestParam(required = false) String color,
+    public ResponseEntity<String> releaseSocksParam(@RequestParam(required = false) String color,
                                                @RequestParam(required = false) Integer size,
                                                @RequestParam(required = false) Integer cottonPart,
                                                @RequestParam(required = false) Integer quantity) {
@@ -131,7 +342,40 @@ public class SocksController {
         }
     }
 
-    @DeleteMapping(consumes = MediaType.APPLICATION_JSON_VALUE)
+    @Operation(
+            summary = "Списание партии бракованных носков со склада",
+            description = "Партия состоит из носков одного цвета, размера и с одинаковым содержанием хлопка"
+    )
+    @Parameters(value = {
+            @Parameter(
+                    name = "apiPairOfSocksDTO",
+                    schema = @Schema(implementation = ApiPairOfSocksDTO.class)
+            )
+    })
+    @ApiResponses(value = {
+            @ApiResponse(
+                    responseCode = "200",
+                    content = {
+                            @Content(mediaType = "text/plain")
+                    },
+                    description = "Носки успешно списаны"
+            ),
+            @ApiResponse(
+                    responseCode = "400",
+                    content = {
+                            @Content(mediaType = "text/plain")
+                    },
+                    description = "Ошибка во входных данных"
+            ),
+            @ApiResponse(
+                    responseCode = "500",
+                    content = {
+                            @Content(mediaType = "text/plain")
+                    },
+                    description = "Внутренняя ошибка сервера"
+            )
+    })
+    @DeleteMapping(path = "/json", consumes = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<String> writeOffSocks(@RequestBody ApiPairOfSocksDTO apiPairOfSocksDTO) {
         try {
             PairOfSocks pairOfSocks = socksService.releaseSocks(apiPairOfSocksDTO);
@@ -147,6 +391,51 @@ public class SocksController {
         }
     }
 
+    @Operation(
+            summary = "Списание партии бракованных носков со склада",
+            description = "Партия состоит из носков одного цвета, размера и с одинаковым содержанием хлопка"
+    )
+    @Parameters(value = {
+            @Parameter(
+                    name = "color",
+                    example = "red"
+            ),
+            @Parameter(
+                    name = "size",
+                    example = "39"
+            ),
+            @Parameter(
+                    name = "cottonPart",
+                    example = "98"
+            ),
+            @Parameter(
+                    name = "quantity",
+                    example = "15"
+            )
+    })
+    @ApiResponses(value = {
+            @ApiResponse(
+                    responseCode = "200",
+                    content = {
+                            @Content(mediaType = "text/plain")
+                    },
+                    description = "Носки успешно списаны"
+            ),
+            @ApiResponse(
+                    responseCode = "400",
+                    content = {
+                            @Content(mediaType = "text/plain")
+                    },
+                    description = "Ошибка во входных данных"
+            ),
+            @ApiResponse(
+                    responseCode = "500",
+                    content = {
+                            @Content(mediaType = "text/plain")
+                    },
+                    description = "Внутренняя ошибка сервера"
+            )
+    })
     @DeleteMapping
     public ResponseEntity<String> writeOffSocks(@RequestParam(required = false) String color,
                                                 @RequestParam(required = false) Integer size,
@@ -167,6 +456,29 @@ public class SocksController {
         }
     }
 
+    @Operation(
+            summary = "Экспорт данных о наличии носков на складе"
+    )
+    @ApiResponses(value = {
+            @ApiResponse(
+                    responseCode = "200",
+                    content = {
+                            @Content(mediaType = MediaType.APPLICATION_JSON_VALUE),
+                    },
+                    description = "JSON-файл успешно сгенерирован и экспортирован"
+            ),
+            @ApiResponse(
+                    responseCode = "204",
+                    description = "Носки на складе отсутствуют"
+            ),
+            @ApiResponse(
+                    responseCode = "500",
+                    content = {
+                            @Content(mediaType = "text/plain")
+                    },
+                    description = "Внутренняя ошибка сервера"
+            )
+    })
     @GetMapping("/export")
     public ResponseEntity<Object> downloadSocksDataFile() {
         try {
@@ -188,6 +500,41 @@ public class SocksController {
         }
     }
 
+    @Operation(
+            summary = "Импорт данных о носках",
+            description = "Импортированные носки заменяют все имеющиеся, носки синхронизируются с операциями"
+    )
+    @Parameters(value = {
+            @Parameter(
+                    name = "file",
+                    content = {
+                            @Content(mediaType = MediaType.MULTIPART_FORM_DATA_VALUE)
+                    }
+            )
+    })
+    @ApiResponses(value = {
+            @ApiResponse(
+                    responseCode = "200",
+                    content = {
+                            @Content(mediaType = "text/plain")
+                    },
+                    description = "Данные о носках успешно импортированы"
+            ),
+            @ApiResponse(
+                    responseCode = "400",
+                    content = {
+                            @Content(mediaType = "text/plain")
+                    },
+                    description = "Ошибка во входных данных"
+            ),
+            @ApiResponse(
+                    responseCode = "500",
+                    content = {
+                            @Content(mediaType = "text/plain")
+                    },
+                    description = "Внутренняя ошибка сервера"
+            )
+    })
     @PutMapping(path = "/import", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
     public ResponseEntity<String> uploadSocksDataFile(@RequestParam MultipartFile file) {
         try {
@@ -200,7 +547,7 @@ public class SocksController {
                     .body("Uploaded file is empty");
         } catch (InvalidInputException | JsonProcessingException e) {
             return ResponseEntity.badRequest()
-                    .body("Invalid file format");
+                    .body(e.toString());
         } catch (IOException e) {
             return ResponseEntity.internalServerError()
                     .body(e.toString());
